32e889919e9b060fdd782decc2b34d3987e52014
Aya Aridah <57573519+Aya-cifar99@users.noreply.github.com>
2021-06-21 20:59:32 +0300

    Create grid.js

---
+++ grid.js
@@ -0,0 +1,44 @@
+const api_url = 'https://jsonplaceholder.typicode.com/photos';
+function createCards(){
+    fetch(api_url).then((response)=>response.json())
+    .then((result)=>{
+        for(let i=0; i< 19; i++){
+            let obj = result[i];
+            let url = obj.url;
+            let title = obj.title;
+            createCard(url, title);             
+        }
+    })
+}
+
+createCards();
+
+function createCard(url, title){
+    
+    let card = document.createElement("div");
+    let img = document.createElement("img");
+    let p = document.createElement("p");
+
+    
+    img.src = url;
+    img.style.width = "30%";
+
+    img.style.cssText = " border-radius: 5px 5px 0 0; max-width: 100%;  vertical-align: middle;";
+    
+    p.innerText = title;
+
+    card.style.cssText = "overflow: hidden; background-color: hsl(0, 0%, 100%); box-shadow: 0 4px 8px 0 rgba(100, 100, 100, 0.2);  text-align: center; border-radius: 5px;    margin-top: 40px; margin-bottom: 20px;"
+
+    card.appendChild(img);
+    card.appendChild(p);
+
+    let cards =  document.getElementById("cards");
+
+    cards.style.display = "grid";
+    cards.style.gridGap = "15px";
+    cards.style.gridTemplateColumns = "repeat(auto-fill,minmax(100px,1fr)";
+    cards.style.margin = '0 auto';
+    cards.style.width = '80%';
+
+    cards.appendChild(card);
+}
